69a33726ce09bd6c8d2ff33439e5a0570f80ba61
Jesús Sánchez de Pablo <jsdp93@gmail.com>
2020-01-29 18:22:32 +0100

    Added readme (expired) and new changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Body extends React.Component {
   render = () => {
-    let body = <div>'Selecciona tu animal'</div>;
+    let body = <div>Selecciona tu animal</div>;
 
     return body;
   };
